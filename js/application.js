88eaa29ffb6cd8e691ac2b921e1fbbddd36570f3
Ilshat Manakhov <ilshat.manahov@gmail.com>
2016-06-09 17:41:57 +0300

    * start

---
+++ js/application.js
@@ -0,0 +1,16 @@
+/**
+ * Created by manahov on 08.06.2016.
+ */
+
+
+var ViewModel = function (first, last) {
+    this.firstName = ko.observable(first);
+    this.lastName = ko.observable(last);
+
+    this.fullName = ko.pureComputed(function () {
+        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
+        return this.firstName() + " " + this.lastName();
+    }, this);
+};
+
+ko.applyBindings(new ViewModel("Planet", "Earth"));
